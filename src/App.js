7ecb4c7a0a7ed671e52220773610808dc10de95a
Abd-ul-Qayyum <a.qayyum1647@gmail.com>
2021-02-22 18:32:39 +0500

    Project
    started project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+    return ( <
+        >
+        <
+        p > Portfolio Aq < /p> <
+        />
+    );
+}
+
+export default App;
